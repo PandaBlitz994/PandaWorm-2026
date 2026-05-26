3764e37840614246f13c9697ba27443fad2e4986
Yiftach Attar <yiftach.main@gmail.com>
2026-05-26 19:44:35 +0300

    button input added and no more millis() that blockes the code
    Updated control logic to simplify pumping and suction sequences. Added button input for battery control.

--- PandaWorm-2026.ino
+++ PandaWorm-2026.ino
@@ -1,126 +1,60 @@
 // pins defenitions
 const byte SUCKTION_PUMPING = 2; // 0 = sucktion / 1 = pumping
-const byte VALVE1_PIN = 3;
-const byte VALVE2_PIN = 4;
-const byte VALVE3_PIN = 5;
-
-const byte TRIG_PIN = 6; // Ultrasonic trig pin
-const byte ECHO_PIN = 7; // Ultrasonic echo pin
-
-const byte BUZZER_PIN = 8; // Buzzer pin
+const byte VALVE1_PIN = 3; // First valve
+const byte VALVE2_PIN = 4; // Second valve
+const byte VALVE3_PIN = 5; // Third valve
+const byte BAT_PIN = 6; // Bttery on/off control pin
+const byte BUTTON_PIN = 8; // Button pin
 
 // Timing variables
 const int spike_time = 50; // Time to avoid electric spikes 
 const int pumping_time = 2000 - spike_time;   // 2 seconds
 const int sucktion_time = 1500 - spike_time;  // 1.5 seconds
 
-const unsigned long sensorInterval = 60;  // Sensor reading delay
-unsigned long seqInterval;                // Pumping/Sucktion delay
-
-// millis variables
-unsigned long prevSeqMillis;
-unsigned long prevSensorMillis;
-unsigned long currentMillis = 0;
-
-// other variables
-int sequenceStep = 0;
-unsigned long dictance; // for containing ultrasonic sensor's result
-
 void setup() {
-  // pneumatics setting
+  // Components setting
   pinMode(SUCKTION_PUMPING, OUTPUT);
   pinMode(VALVE1_PIN, OUTPUT);
   pinMode(VALVE2_PIN, OUTPUT);
   pinMode(VALVE3_PIN, OUTPUT);
+  pinMode(BAT_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  // ultrasonic setting
-  pinMode(TRIG_PIN, OUTPUT); 
-  pinMode(ECHO_PIN, INPUT);
-
-  // buzzer setting
-  pinMode(BUZZER_PIN, OUTPUT);
-
-  // Reset before entering the big big loop
-  seqInterval = sucktion_time; // start at sucktion
-  prevSensorMillis = millis();
-  prevSeqMillis = millis();
+  while (digitalRead(BUTTON_PIN)); // Waiting for the button to be pressed before passing to the loop
+  delay(100);
+  digitalWrite(BAT_PIN, 1);
 }
 
 void loop() {
-  currentMillis = millis(); // Resetting the current time
+  // --- pumping ---
+  // First vertebra
+  pumpingReset(); // Enter pumping mode
+  digitalWrite(VALVE1_PIN, 0); // Navigating the pump to the first vertebra
+  delay(pumping_time);
+  // Second vertebra 
+  digitalWrite(VALVE1_PIN, 1); // Navigating the pump to the second vertebra
+  delay(pumping_time);
+  // Third vertebra 
+  digitalWrite(VALVE2_PIN, 1); // Navigating the pump to the third vertebra
+  delay(pumping_time);
+  // Fourth vertebra 
+  digitalWrite(VALVE3_PIN, 1); // Navigating the pump to the fourth vertebra
+  delay(pumping_time);
 
-  if (currentMillis - prevSeqMillis >= seqInterval) {
-    prevSeqMillis = currentMillis;
-
-    switch (sequenceStep) {
-      // --- sucktion ---
-      // First vertebra
-      case 0:
-        sucktionReset(); // Enter sucktion mode
-        digitalWrite(VALVE1_PIN, 0); // Navigating the pump to the first vertebra
-        break;
-      // Second vertebra 
-      case 1:
-        digitalWrite(VALVE1_PIN, 1); // Navigating the pump to the second vertebra
-        break;
-      // Third vertebra 
-      case 2:
-        digitalWrite(VALVE2_PIN, 1); // Navigating the pump to the third vertebra
-        break;
-      // Fourth vertebra 
-      case 3:
-        digitalWrite(VALVE3_PIN, 1); // Navigating the pump to the fourth vertebra
-        break;
-
-      // --- pumping ---
-      // First vertebra
-      case 4:
-        pumpingReset(); // Enter pumping mode
-        digitalWrite(VALVE1_PIN, 0); // Navigating the pump to the first vertebra
-        break;
-      // Second vertebra 
-      case 5:
-        digitalWrite(VALVE1_PIN, 1); // Navigating the pump to the second vertebra
-        break;
-      // Third vertebra 
-      case 6:
-        digitalWrite(VALVE2_PIN, 1); // Navigating the pump to the third vertebra
-        break;
-      // Fourth vertebra 
-      case 7:
-        digitalWrite(VALVE3_PIN, 1); // Navigating the pump to the fourth vertebra
-        break;
-    }
-    sequenceStep ++; // Passing to the next case
-    if (sequenceStep > 7) { // Reseting the switc case in case it ended
-      sequenceStep = 0;
-    }
-  }
-
-  if (currentMillis - prevSensorMillis >= sensorInterval) {
-    prevSensorMillis = currentMillis;
-
-    dictance = readUltrasonic();
-    if (dictance < 15) {              // if somthing is detected beep
-      digitalWrite(BUZZER_PIN, 1);
-    }
-    else{
-      digitalWrite(BUZZER_PIN, 0);    // if nothing detected don't beep
-    }
-  }
-  delay(spike_time); // Avoiding electrical spikes
-}
-
-
-long readUltrasonic() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  
-  long duration = pulseIn(ECHO_PIN, HIGH);
-  return duration * 0.034 / 2; // Returns distance in cm
+  // --- sucktion ---
+  // First vertebra
+  sucktionReset(); // Enter sucktion mode
+  digitalWrite(VALVE1_PIN, 0); // Navigating the pump to the first vertebra
+  delay(sucktion_time);
+  // Second vertebra
+  digitalWrite(VALVE1_PIN, 1); // Navigating the pump to the second vertebra
+  delay(sucktion_time);
+  // Third vertebra 
+  digitalWrite(VALVE2_PIN, 1); // Navigating the pump to the third vertebra
+  delay(sucktion_time);
+  // Fourth vertebra 
+  digitalWrite(VALVE3_PIN, 1); // Navigating the pump to the fourth vertebra
+  delay(sucktion_time);
 }
 
 
@@ -130,7 +64,6 @@ void sucktionReset() { // Reset to enter suction mode
   digitalWrite(VALVE2_PIN, 0);
   digitalWrite(VALVE3_PIN, 0);
   digitalWrite(SUCKTION_PUMPING, 0);
-  seqInterval = sucktion_time; // Resetting the seqInterval to sucktion mode
 }
 
 void pumpingReset(){ // Reset to enter pumping mode
@@ -138,5 +71,4 @@ void pumpingReset(){ // Reset to enter pumping mode
   digitalWrite(VALVE2_PIN, 0);
   digitalWrite(VALVE3_PIN, 0);
   digitalWrite(SUCKTION_PUMPING, 1);
-  seqInterval = pumping_time; // Resetting the seqInterval to pumping mode
 }
